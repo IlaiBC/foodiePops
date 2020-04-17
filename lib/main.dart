e631a3fd8c0853b345375b80746d42d1ccc54380
Guy <guy.oxnb@gmail.com>
2020-04-17 17:53:04 +0300

    mid work commit - improved login flow and added an infrastructure with multiProvider to handle firebase DB

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:foodiepops/loginScreen.dart';
 import 'package:foodiepops/mainScreen.dart';
+import 'package:foodiepops/services/firebaseAuthService.dart';
+import 'package:foodiepops/services/fireStoreDatabase.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+    const MyApp({Key key, this.authServiceBuilder, this.databaseBuilder})
+      : super(key: key);
+  // Expose builders for 3rd party services at the root of the widget tree
+  // This is useful when mocking services while testing
+  final FirebaseAuthService Function(BuildContext context) authServiceBuilder;
+  final FirestoreDatabase Function(BuildContext context, String uid)
+      databaseBuilder;
+      
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
