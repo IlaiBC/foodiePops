f87429814ceac3be745ed1e79fac588932721ab4
IlaiBC <ilai.ben-chetrit@fyber.com>
2020-06-21 13:50:08 +0300

    onboarding first entry differntiator wip

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:foodiepops/services/imagePickerService.dart';
 import 'package:provider/provider.dart';
 import 'authentication/authWidgetBuilder.dart';
 import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() => runApp(MyApp(
       authServiceBuilder: (_) => FirebaseAuthService(),
@@ -15,13 +16,13 @@ void main() => runApp(MyApp(
     ));
 
 class MyApp extends StatelessWidget {
-    const MyApp({Key key, this.authServiceBuilder, this.databaseBuilder})
+  const MyApp({Key key, this.authServiceBuilder, this.databaseBuilder})
       : super(key: key);
+
   // Expose builders for 3rd party services at the root of the widget tree
   // This is useful when mocking services while testing
   final FirebaseAuthService Function(BuildContext context) authServiceBuilder;
-  final FirestoreDatabase Function(BuildContext context)
-      databaseBuilder;
+  final FirestoreDatabase Function(BuildContext context) databaseBuilder;
 
   @override
   Widget build(BuildContext context) {
@@ -49,10 +50,53 @@ class MyApp extends StatelessWidget {
               '/auth': (context) => AuthWidget(userSnapshot: userSnapshot),
             },
             title: 'FoodiePops',
-            theme: ThemeData(primarySwatch: Colors.red, primaryColor: Color(0xffe51923)),
-            home: OnboardingScreen(title: 'Fancy OnBoarding HomePage'),
+            theme: ThemeData(
+                primarySwatch: Colors.red, primaryColor: Color(0xffe51923)),
+            home: Splash(),
           );
         },
+      ),
+    );
+  }
+}
+
+class Splash extends StatefulWidget {
+
+  @override
+  SplashState createState() => new SplashState();
+}
+
+class SplashState extends State<Splash> {
+  AsyncSnapshot<User> _userSnapshot;
+
+  @override
+  void initState() {
+    super.initState();
+    checkFirstSeen();
+  }
+
+  Future checkFirstSeen() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool _seen = (prefs.getBool('seen') ?? false);
+    debugPrint(_seen.toString());
+    debugPrint("here");
+
+    if (_seen) {
+      Navigator.of(context).pushReplacementNamed('/auth');
+    } else {
+      await prefs.setBool('seen', true);
+      Navigator.of(context).pushReplacement(new MaterialPageRoute(
+          builder: (context) =>
+              OnboardingScreen(title: 'Fancy OnBoarding HomePage')));
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Center(
+        child: new Text('Loading...'),
       ),
     );
   }
