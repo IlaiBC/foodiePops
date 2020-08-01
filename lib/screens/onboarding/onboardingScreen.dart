670c16d8c0ec67026c7fbf34ebc3e8970539c09f
IlaiBC <ilai.ben-chetrit@fyber.com>
2020-08-01 11:40:26 +0300

    wip

--- lib/screens/onboarding/onboardingScreen.dart
+++ lib/screens/onboarding/onboardingScreen.dart
@@ -22,12 +22,14 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
               color: Colors.white,
               fontSize: 34.0,
             )),
-        body: Text('Find special foodie expiriences near you',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18.0,
-            )),
+        body: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Text('Find special foodie expiriences near you',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                ))),
         iconAssetPath: 'assets/foodie.png'),
     PageModel(
         color: const Color(0xFF65B0B4),
@@ -38,12 +40,14 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
               color: Colors.white,
               fontSize: 34.0,
             )),
-        body: Text('Get updated on the latest news in the foodie scene',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18.0,
-            )),
+        body: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Text('Get updated on the latest news in the foodie scene',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                ))),
         iconAssetPath: 'assets/news.png'),
     PageModel(
       color: const Color(0xFF9B90BC),
@@ -54,13 +58,14 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             color: Colors.white,
             fontSize: 34.0,
           )),
-      body: Text(
-          'Login in to FoodiePops in the profile page for a more personalized expirience',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 18.0,
-          )),
+      body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: Text('Login in the profile page to redeem pop coupons',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+              ))),
       iconAssetPath: 'assets/profile.png',
     ),
     PageModel(
@@ -72,13 +77,15 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             color: Colors.white,
             fontSize: 34.0,
           )),
-      body: Text(
-          'FoodiePops uses your location data to customize pop offers for you',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 18.0,
-          )),
+      body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: Text(
+              'FoodiePops uses your location data to customize pop offers for you',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18.0,
+              ))),
       iconAssetPath: 'assets/location.png',
     ),
   ];
