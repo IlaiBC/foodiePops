1002f1323c062ef85adb9aeecc4af23f1f6e41d3
IlaiBC <ilai.ben-chetrit@fyber.com>
2020-08-01 18:06:29 +0300

    wip

--- lib/screens/businessUser/CredentialsLoginScreen.dart
+++ lib/screens/businessUser/CredentialsLoginScreen.dart
@@ -3,37 +3,41 @@ import 'package:foodiepops/constants/Texts.dart';
 import 'package:foodiepops/screens/login/credentialsLogin/credentialsLoginForm.dart';
 
 class CredentialsLoginScreen extends StatelessWidget {
-
   CredentialsLoginScreen({Key key, this.isBusinessUser}) : super(key: key);
   final bool isBusinessUser;
 
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
+    return Scaffold(
       appBar: AppBar(
         elevation: 2.0,
-        title: Text(isBusinessUser ? Texts.businessCredentialsLogin : Texts.userCredentialsLogin),
+        title: Text(isBusinessUser
+            ? Texts.businessCredentialsLogin
+            : Texts.userCredentialsLogin),
       ),
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-        Padding(
-        padding: const EdgeInsets.only(top: 50.0),
-        child: Image(
-          image: AssetImage("assets/foodiepopslogo_no_bg.png"),
-          height: 250)),
+      body: new GestureDetector(
+          onTap: () {
+            FocusScope.of(context).requestFocus(new FocusNode());
+          },
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                    padding: const EdgeInsets.only(top: 50.0),
+                    child: Image(
+                        image: AssetImage("assets/foodiepopslogo_no_bg.png"),
+                        height: 250)),
                 SizedBox(height: 10),
-
-          Card(
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: CredentialsSignInFormBuilder(isBusinessUser: isBusinessUser)
+                Card(
+                  child: Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: CredentialsSignInFormBuilder(
+                          isBusinessUser: isBusinessUser)),
+                ),
+              ],
             ),
-          ),
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
